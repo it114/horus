bb56966d049e2e8eff9b86b24ed2456edb44f19d
delta24 <viyat001@gmail.com>
2015-10-04 22:03:41 +0530

    Dummy about, dashboard pages

--- mobsec/controllers/main.py
+++ mobsec/controllers/main.py
@@ -1,4 +1,7 @@
+import os
 from flask import Blueprint, render_template, flash, request, redirect, url_for
+from werkzeug import secure_filename
+
 from mobsec.extensions import cache
 
 
@@ -9,3 +12,20 @@ main = Blueprint('main', __name__)
 def home():
     return render_template('index.html')
 
+@main.route('/about')
+@cache.cached(timeout=1000)
+def about():
+    return render_template('about.html')
+
+@main.route("/", methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        apk = request.files['apk']
+        if apk:
+            app_name = secure_filename(apk)
+            apk.save()
+            return redirect(url_for("dashboard"))
+
+@main.route("/dashboard", methods=['GET'])
+def dashboard():
+    return render_template('dashboard.html')
