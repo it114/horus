694f3784659af10a052005d625edfecd9571d7ae
delta24 <viyat001@gmail.com>
2015-10-04 17:43:28 +0530

    Added boilerplate code

---
+++ README.md
@@ -0,0 +1,11 @@
+## Mobsec
+
+A mobile pentesting framework written in Python
+
+## CREDITS
+
+@ajinabraham for the [MobSF](https://github.com/ajinabraham/Mobile-Security-Framework-MobSF).
+
+## LICENSE
+
+See the [LICENSE](https://github.com/delta24/mobsec/LICENSE) file
---
+++ mobsec/settings.py
@@ -0,0 +1,33 @@
+import tempfile
+db_file = tempfile.NamedTemporaryFile()
+
+
+class Config(object):
+    SECRET_KEY = '#r$=rg*lit&!4nukg++@%k+n9#6fhkv_*a6)2t$n1b=*sadwq'
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../mobsec.db'
+
+    CACHE_TYPE = 'simple'
+
+
+class DevConfig(Config):
+    DEBUG = True
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../mobsec.db'
+
+    CACHE_TYPE = 'null'
+    ASSETS_DEBUG = True
+
+
+class TestConfig(Config):
+    DEBUG = True
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_file.name
+    SQLALCHEMY_ECHO = True
+
+    CACHE_TYPE = 'null'
+    WTF_CSRF_ENABLED = False
