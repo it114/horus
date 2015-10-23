178aa08d754aeb7bc8454e9952c668e8bf497558
delta24 <viyat001@gmail.com>
2015-10-23 23:04:01 +0530

    New tools archive

--- install_tools.sh
+++ install_tools.sh
@@ -3,7 +3,7 @@
 TOOLS_URL=https://www.dropbox.com/s/d8y5skuzai9j2gk/tools.zip?dl=0
 
 wget ${TOOLS_URL} -o tools.zip
-
+mkdir tools/
 unzip -d tools/ tools.zip
 rm tools.zip
 echo "Tools installed successfully!"
