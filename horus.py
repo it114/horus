e35d7d7da7df52289112c8af1950a07eb9b571df
delta24 <viyat001@gmail.com>
2016-01-11 13:37:23 +0530

    Modified requirements file

--- horus.py
+++ horus.py
@@ -2,15 +2,15 @@ from framework.interface import server
 
 
 def banner():
-    print("""\033[92m
+    print """\033[92m
 
-    \033[0m""")
+    \033[0m"""
 
 def start_interface_server():
     print("starting the server...")
     server.main()
 
 
-
 if __name__ == "__main__":
+    banner()
     start_interface_server()
